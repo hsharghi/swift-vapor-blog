88af4a73a5b1ae77edd74db7084a64d3b32e4391
Hadi Sharghi <hsharghi@gmail.com>
2019-12-20 22:52:51 +0330

    create Post model and conform to basic protocols

---
+++ Sources/App/Models/Post.swift
@@ -0,0 +1,28 @@
+//
+//  Post.swift
+//  App
+//
+//  Created by Hadi Sharghi on 12/20/19.
+//
+
+import Vapor
+import FluentSQLite    // 1
+
+
+final class Post: SQLiteModel {        // 2
+    var id: Int?
+    var title: String
+    var body: String    // 3
+    
+    init(id: Int? = nil, title: String, body: String) {    // 4
+        self.id = id
+        self.title = title
+        self.body = body
+    }
+
+}
+
+extension Post: Migration { } // 5
+extension Post: Content { }   // 6
+extension Post: Parameter { } // 7
+
