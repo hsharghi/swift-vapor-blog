7daf137bad4fb05ba3df94000de67686d34b2880
Hadi Sharghi <hsharghi@gmail.com>
2019-12-20 22:55:33 +0330

    add CRUD routes for PostController

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,20 +1,22 @@
 import Vapor
 
-/// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
-    
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+        let postController = PostController() // 1
+  
+        // 2
+        /// GET /posts
+        router.get("posts", use: postController.index)
+    
+        /// GET /posts/:id
+        router.get("posts", Post.parameter, use: postController.show)
+    
+        /// POST /posts
+        router.post("posts", use: postController.create)
+    
+        /// PATCH /posts/:id
+        router.patch("posts", Post.parameter, use: postController.update)
+    
+        /// DELETE /posts/:id
+        router.delete("posts", Post.parameter, use: postController.delete)
 }
